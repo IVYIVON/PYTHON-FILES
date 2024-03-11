f891e8b1ec5b5ec99f9bc02a8f649bf1ce57b160
IVYIVON <159934783+IVYIVON@users.noreply.github.com>
2024-03-11 20:27:21 +0300

    Add files via upload

---
+++ user.input.py
@@ -0,0 +1,9 @@
+# user_input.py
+
+# Pre-defined values
+name = "Ivon"
+age = 23
+location = "Nairobi"
+
+# Printing personalized message
+print(f"Hello {name}, you are {age} years old and live in {location}.")
